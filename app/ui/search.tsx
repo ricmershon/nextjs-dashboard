4c4b0fd95d71b34a54e1f44d8f5784ee99d5b172
Ric Mershon <rmersh@ricmershon.com>
2024-09-09 16:23:53 -0400

    Pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -16,6 +16,7 @@ export default function Search({ placeholder }: SearchProps) {
 
     const handleSearch = useDebouncedCallback((term) => {
         const params = new URLSearchParams(searchParams);
+        params.set('page', '1');
         if (term) {
             params.set('query', term);
         } else {
