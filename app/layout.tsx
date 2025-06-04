508a495b4c8fc72c214fad11eb1e8e17e0606433
Ric Mershon <rmersh@ricmershon.com>
2025-06-04 18:01:31 -0400

    global styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
+import '@/app/ui/global.css';
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
     <html lang="en">
-      <body>{children}</body>
+        <body>{children}</body>
     </html>
-  );
-}
+);
+
+export default RootLayout;
